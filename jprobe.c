0f0cc9b9aab614a3e9986673fa978f7d96273490
ferar achkar <ferarachkar@gmail.com>
2017-08-12 20:22:23 +0300

    stashing latest changes
    this a more complete jprobe and retprobe impl.
    --------------------------

--- jprobe.c
+++ jprobe.c
@@ -23,6 +23,9 @@
 #include <linux/rcupdate.h>             /* rcu related */
 #include "../../drivers/gpio/gpiolib.h" /* ../../ from current location of kprobeo.c !*/
 #include <linux/kallsyms.h>
+#include "../../drivers/base/base.h"
+#include <linux/async.h>
+#include "../../drivers/pinctrl/core.h"
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("This is  jprobe testbench ;/");
@@ -34,102 +37,188 @@ MODULE_VERSION("10.4.0");
 /* NOTE: http://www.tldp.org/HOWTO/KernelAnalysis-HOWTO-14.html */
 static struct jprobe  *ptr_my_jp = NULL;
 static struct jprobe  *ptr_my_jp2 = NULL;
-
-/* Proxy routine having the same arguments as actual gpiod_request() routine */
-static long jp_my_callback(struct gpio_chip *chip, unsigned offset, int value)
-{
-    static ktime_t entering, exiting;
-    static s64 deltao = 0;
-    trace_printk(" >> entering %s\n", __func__);
-    entering = ktime_get();
-
-    trace_printk("    %s, offset:%u, value:%d\n", __func__, offset, value);
-    trace_printk("    %s, chip -- label:%s, base:%d, ngpio:%u\n", __func__, chip->label, chip->base, chip->ngpio);
-
-    exiting = ktime_get();
-    deltao = ktime_to_ns(ktime_sub(exiting, entering));
-    trace_printk(" << exiting %s, time took : %lld ns\n", __func__, deltao);
-    /* Always end with a call to jprobe_return(). */
-    jprobe_return();
-    return 0;
-}
-
-
+static struct kretprobe *ptr_my_kretp = NULL;
+static const char my_func_name[] = "driver_probe_device";
+static const char my_func2_name[] = "__driver_attach";
 struct gpio_regs {
-        u32 irqenable1;
-        u32 irqenable2;
-        u32 wake_en;
-        u32 ctrl;
-        u32 oe;
-        u32 leveldetect0;
-        u32 leveldetect1;
-        u32 risingdetect;
-        u32 fallingdetect;
-        u32 dataout;
-        u32 debounce;
-        u32 debounce_en;
+    u32 irqenable1;
+    u32 irqenable2;
+    u32 wake_en;
+    u32 ctrl;
+    u32 oe;
+    u32 leveldetect0;
+    u32 leveldetect1;
+    u32 risingdetect;
+    u32 fallingdetect;
+    u32 dataout;
+    u32 debounce;
+    u32 debounce_en;
 };
 
 
 struct gpio_bank {
-        struct list_head node;
-        void __iomem *base;
-        int irq;
-        u32 non_wakeup_gpios;
-        u32 enabled_non_wakeup_gpios;
-        struct gpio_regs context;
-        u32 saved_datain;
-        u32 level_mask;
-        u32 toggle_mask;
-        raw_spinlock_t lock;
-        raw_spinlock_t wa_lock;
-        struct gpio_chip chip;
-        struct clk *dbck;
-        u32 mod_usage;
-        u32 irq_usage;
-        u32 dbck_enable_mask;
-        bool dbck_enabled;
-        bool is_mpuio;
-        bool dbck_flag;
-        bool loses_context;
-        bool context_valid;
-        int stride;
-        u32 width;
-        int context_loss_count;
-        int power_mode;
-        bool workaround_enabled;
+    struct list_head node;
+    void __iomem *base;
+    int irq;
+    u32 non_wakeup_gpios;
+    u32 enabled_non_wakeup_gpios;
+    struct gpio_regs context;
+    u32 saved_datain;
+    u32 level_mask;
+    u32 toggle_mask;
+    raw_spinlock_t lock;
+    raw_spinlock_t wa_lock;
+    struct gpio_chip chip;
+    struct clk *dbck;
+    u32 mod_usage;
+    u32 irq_usage;
+    u32 dbck_enable_mask;
+    bool dbck_enabled;
+    bool is_mpuio;
+    bool dbck_flag;
+    bool loses_context;
+    bool context_valid;
+    int stride;
+    u32 width;
+    int context_loss_count;
+    int power_mode;
+    bool workaround_enabled;
 
-        void (*set_dataout)(struct gpio_bank *bank, unsigned gpio, int enable);
-        int (*get_context_loss_count)(struct device *dev);
+    void (*set_dataout)(struct gpio_bank *bank, unsigned gpio, int enable);
+    int (*get_context_loss_count)(struct device *dev);
 
-        struct omap_gpio_reg_offs *regs;
+    struct omap_gpio_reg_offs *regs;
 };
 
-static long jp_my2_callback(struct gpio_bank *bank, unsigned offset, int enable)
+/* Proxy routine having the same arguments as actual gpiod_request() routine */
+static long jp_my_callback(struct device_driver *drv, struct device *dev)
 {
+    static ktime_t entering, exiting;
+    static s64 deltao = 0;
+    trace_printk(" >> entering %s\n", my_func_name);
+    entering = ktime_get();
+
+    if (drv->probe != NULL)
+        trace_printk("    %s, probe:%pF @ %p\n", my_func_name, drv->probe, drv->probe);
+
+    trace_printk("    %s, drivers_autoprobe:%d\n", my_func_name, drv->bus->p->drivers_autoprobe);
+
+    trace_printk("    %s, name: %s, probe_type:%d\n", my_func_name, drv->name, drv->probe_type);
+
+    if (dev->of_node != NULL)
+        trace_printk("   of_node - name:%s, type:%s, full_name:%s\n", dev->of_node->name, dev->of_node->type, dev->of_node->full_name);
+
+
+    if (dev != NULL)
+        trace_printk("   device init_name:%s, state_in_sysfs:%d\n", dev->init_name, dev->kobj.state_in_sysfs);
+
+    if (dev->type != NULL)
+        trace_printk("          type-name:%s\n",   dev->type->name);
+
+    if (dev->driver != NULL)
+        trace_printk("          drv-name:%s\n", dev->driver->name);
+
+
+
+    if (dev->pins != NULL)
+        if (dev->pins->p != NULL)
+            if (dev->pins->p->dev != NULL) {
+                trace_printk("   pinctrl device init_name:%s\n", dev->pins->p->dev->init_name);
+
+                if (dev->pins->p->dev->type != NULL)
+                    trace_printk("           device type-name:%s\n",   dev->pins->p->dev->type->name);
+
+                if (dev->pins->p->dev->driver != NULL)
+                    trace_printk("           device drv-name:%s\n", dev->pins->p->dev->driver->name);
+            }
+
+    exiting = ktime_get();
+    deltao = ktime_to_ns(ktime_sub(exiting, entering));
+    trace_printk(" << exiting %s, time took : %lld ns\n", my_func_name, deltao);
+    /* Always end with a call to jprobe_return(). */
+    jprobe_return();
+    return 0;
+}
+
+static long jp_my2_callback(struct device *dev, void *data)
+{
+    struct device_driver *drv = data;
+    static ktime_t entering, exiting;
+    static s64 deltao = 0;
+    trace_printk(" >> entering %s\n", my_func2_name);
+    entering = ktime_get();
+
+
+    if (drv->probe != NULL)
+        trace_printk("    %s, probe:%pF @ %p\n", my_func2_name, drv->probe, drv->probe);
+
+    if (drv->bus != NULL)
+        trace_printk("    %s, match:%pF @ %p\n", my_func2_name, drv->bus->match, drv->bus->match);
+
+    trace_printk("    %s, drivers_autoprobe:%d\n", my_func2_name, drv->bus->p->drivers_autoprobe);
+
+    trace_printk("    %s, name: %s, probe_type:%d\n", my_func2_name, drv->name, drv->probe_type);
+
+    if (dev->of_node != NULL)
+        trace_printk("   of_node - name:%s, type:%s, full_name:%s\n", dev->of_node->name, dev->of_node->type, dev->of_node->full_name);
+
+    if (dev->parent != NULL)
+        trace_printk("   parent - init_name:%s, mutex:%p, locked:%d\n", dev->parent->init_name, &dev->parent->mutex, mutex_is_locked(&dev->parent->mutex));
+
+    if (dev != NULL)
+        trace_printk("   device init_name:%s, state_in_sysfs:%d, mutex:%p\n", dev->init_name, dev->kobj.state_in_sysfs, &dev->mutex);
+
+    if (dev->type != NULL)
+        trace_printk("          type-name:%s\n",   dev->type->name);
+
+    if (dev->driver != NULL)
+        trace_printk("          drv-name:%s\n", dev->driver->name);
+    else
+        trace_printk("     *** dev->driver = NULL should call driver_probe_device ***\n");
+
+
+
+    if (dev->pins != NULL)
+        if (dev->pins->p != NULL)
+            if (dev->pins->p->dev != NULL) {
+                trace_printk("   pinctrl device init_name:%s\n", dev->pins->p->dev->init_name);
+
+                if (dev->pins->p->dev->type != NULL)
+                    trace_printk("           device type-name:%s\n",   dev->pins->p->dev->type->name);
+
+                if (dev->pins->p->dev->driver != NULL)
+                    trace_printk("           device drv-name:%s\n", dev->pins->p->dev->driver->name);
+            }
+
+//     if (chip->request != NULL)
+//         trace_printk("    %s, chip -- request:%pF @ %p\n", my_func2_name, chip->request, chip->request);
+    exiting = ktime_get();
+    deltao = ktime_to_ns(ktime_sub(exiting, entering));
+    trace_printk(" << exiting %s, time took : %lld ns\n", my_func2_name, deltao);
+    /* Always end with a call to jprobe_return(). */
+    jprobe_return();
+    return 0;
+}
+
+
+static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+    unsigned long retval = regs_return_value(regs);
     static ktime_t entering, exiting;
     static s64 deltao = 0;
     trace_printk(" >> entering %s\n", __func__);
     entering = ktime_get();
 
-    trace_printk("    %s, offset:%u, enable:%d\n", __func__, offset, enable);
-    if (bank->base != NULL)
-        trace_printk("    %s, bank->base:%p\n", __func__, bank->base);
-    trace_printk("    %s, bank->irq:%d, bank->saved_datain:%u\n", __func__, bank->irq, bank->saved_datain);
-    trace_printk("    %s, bank->level_mask:%u, bank->toggle_mask:%u\n", __func__, bank->level_mask, bank->toggle_mask);
-    trace_printk("    %s, bank->mod_usage:%u, bank->irq_usage:%u, bank->dbck_enable_mask:%u\n", __func__, bank->mod_usage, bank->irq_usage, bank->dbck_enable_mask);
+    trace_printk("%s returned %lu\n", ri->rp->kp.symbol_name, retval);
 
-//     trace_printk("    %s, chip -- can sleep:%d\n", __func__, chip->can_sleep);
-//     trace_printk("    %s, bgpio_data:%lu, bgpio_dir:%lu, bgpio_bits:%d  \n", __func__, chip->bgpio_data, chip->bgpio_dir, chip->bgpio_bits);
-//     if (chip->request != NULL)
-//         trace_printk("    %s, chip -- request:%pF @ %p\n", __func__, chip->request, chip->request);
     exiting = ktime_get();
     deltao = ktime_to_ns(ktime_sub(exiting, entering));
     trace_printk(" << exiting %s, time took : %lld ns\n", __func__, deltao);
-    /* Always end with a call to jprobe_return(). */
-    jprobe_return();
+
+
     return 0;
 }
+
 
 
 int __init my_probe_init(void)
@@ -146,28 +235,16 @@ int __init my_probe_init(void)
         return -ENOMEM; // or  PTR_ERR(ptr_my_jp)
     }
 
-
-    ptr_my_jp2 = kzalloc(sizeof(struct jprobe), GFP_KERNEL);
-    if (IS_ERR(ptr_my_jp2)) {
-        trace_printk("warning %s:%d, ret:%ld\n", __func__, __LINE__, PTR_ERR(ptr_my_jp2));
-        if (ptr_my_jp != NULL) {
-            kfree(ptr_my_jp);
-            ptr_my_jp = NULL;
-        }
-        return -ENOMEM; // or  PTR_ERR(ptr_my_jp2)
-    }
-
-
     ptr_my_jp->entry = jp_my_callback;
     /* specify the address/offset where you want to insert probe.
      * You can get the address using one of the methods described above.
      */
-    ptr_my_jp->kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name("omap_gpio_set");
+    ptr_my_jp->kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name(my_func_name);
 
     /* check if the kallsyms_lookup_name() returned the correct value.
      */
     if (ptr_my_jp->kp.addr == NULL) {
-        trace_printk("warning kallsyms_lookup_name could not find address for the specified symbol %s\n", "omap_gpio_set");
+        trace_printk("warning kallsyms_lookup_name could not find address for the specified symbol %s\n", my_func_name);
         if (ptr_my_jp != NULL) {
             kfree(ptr_my_jp);
             ptr_my_jp = NULL;
@@ -187,19 +264,30 @@ int __init my_probe_init(void)
         return ret;
     }
 
+    /// ptr_my_jp2
 
+
+    ptr_my_jp2 = kzalloc(sizeof(struct jprobe), GFP_KERNEL);
+    if (IS_ERR(ptr_my_jp2)) {
+        trace_printk("warning %s:%d, ret:%ld\n", __func__, __LINE__, PTR_ERR(ptr_my_jp2));
+        if (ptr_my_jp != NULL) {
+            kfree(ptr_my_jp);
+            ptr_my_jp = NULL;
+        }
+        return -ENOMEM; // or  PTR_ERR(ptr_my_jp2)
+    }
 
 
     ptr_my_jp2->entry = jp_my2_callback;
     /* specify the address/offset where you want to insert probe.
      * You can get the address using one of the methods described above.
      */
-    ptr_my_jp2->kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name("omap_set_gpio_dataout_reg");
+    ptr_my_jp2->kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name(my_func2_name);
 
     /* check if the kallsyms_lookup_name() returned the correct value.
      */
     if (ptr_my_jp2->kp.addr == NULL) {
-        trace_printk("warning kallsyms_lookup_name could not find address for the specified symbol %s\n", "omap_set_gpio_dataout_reg");
+        trace_printk("warning kallsyms_lookup_name could not find address for the specified symbol %s\n", my_func2_name);
         if (ptr_my_jp != NULL) {
             unregister_jprobe(ptr_my_jp);
             kfree(ptr_my_jp);
@@ -231,6 +319,52 @@ int __init my_probe_init(void)
         return ret;
     }
 
+
+    /// ptr_my_kretp
+
+    ptr_my_kretp = kzalloc(sizeof(struct kretprobe), GFP_KERNEL);
+    if (IS_ERR(ptr_my_kretp)) {
+        trace_printk("warning %s:%d, ret:%ld\n", __func__, __LINE__, PTR_ERR(ptr_my_kretp));
+        if (ptr_my_jp != NULL) {
+            unregister_jprobe(ptr_my_jp);
+            kfree(ptr_my_jp);
+            ptr_my_jp = NULL;
+        }
+
+        if (ptr_my_jp2 != NULL) {
+            unregister_jprobe(ptr_my_jp2);
+            kfree(ptr_my_jp2);
+            ptr_my_jp2 = NULL;
+        }
+
+        return -ENOMEM; // or  PTR_ERR(ptr_my_jp2)
+    }
+
+    ptr_my_kretp->kp.symbol_name = "platform_match";
+    ptr_my_kretp->handler = ret_handler;
+    ptr_my_kretp->maxactive = 5;
+    ret = register_kretprobe(ptr_my_kretp);
+    if (ret < 0) {
+        trace_printk("warning %s:%d, register_kretprobe failed, returned %d\n", __func__, __LINE__, ret);
+        if (ptr_my_jp != NULL) {
+            unregister_jprobe(ptr_my_jp);
+            kfree(ptr_my_jp);
+            ptr_my_jp = NULL;
+        }
+
+        if (ptr_my_jp2 != NULL) {
+            unregister_jprobe(ptr_my_jp2);
+            kfree(ptr_my_jp2);
+            ptr_my_jp2 = NULL;
+        }
+
+        if (ptr_my_kretp != NULL) {
+            kfree(ptr_my_kretp);
+            ptr_my_kretp = NULL;
+        }
+        return ret;
+    }
+
     exiting = ktime_get();
     deltao = ktime_to_ns(ktime_sub(exiting, entering));
     trace_printk(" << exiting %s, time took : %lld ns\n", __func__, deltao);
@@ -245,6 +379,12 @@ void __exit my_probe_exit(void)
     static s64 deltao = 0;
     entering = ktime_get();
     trace_printk("  >> entering %s\n", __func__);
+
+    if (ptr_my_kretp != NULL) {
+        unregister_kretprobe(ptr_my_kretp);
+        kfree(ptr_my_kretp);
+        ptr_my_kretp = NULL;
+    }
 
     if (ptr_my_jp2 != NULL) {
 
